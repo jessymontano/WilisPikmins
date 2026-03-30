51d727adfec5c89806549a63b7449502edfe0239
jessica <jessymontano@hotmail.com>
2026-03-30 09:55:25 -0700

    add crafting recipe for whistle and charging horn

--- src/main/java/net/wili/wilispikmins/datagen/DataGenerators.java
+++ src/main/java/net/wili/wilispikmins/datagen/DataGenerators.java
@@ -33,6 +33,7 @@ public class DataGenerators {
                 List.of(new LootTableProvider.SubProviderEntry(ModBlockLootTableProvider::new, LootContextParamSets.BLOCK)), lookupProvider));
         generator.addProvider(event.includeServer(), new ModItemTagProvider(packOutput, lookupProvider, blockTagsProvider.contentsGetter(), existingFileHelper));
         generator.addProvider(event.includeServer(), new ModAdvancementsProvider(packOutput, lookupProvider, existingFileHelper));
+        generator.addProvider(event.includeServer(), new ModRecipeProvider(packOutput, lookupProvider));
 
         generator.addProvider(event.includeClient(), new ModBlockStateProvider(packOutput, existingFileHelper));
         generator.addProvider(event.includeClient(), new ModItemModelProvider(packOutput, existingFileHelper));
--- src/main/java/net/wili/wilispikmins/datagen/ModRecipeProvider.java
+++ src/main/java/net/wili/wilispikmins/datagen/ModRecipeProvider.java
@@ -1,4 +1,39 @@
 package net.wili.wilispikmins.datagen;
 
-public class ModRecipeProvider {
+import net.minecraft.core.HolderLookup;
+import net.minecraft.data.PackOutput;
+import net.minecraft.data.recipes.RecipeCategory;
+import net.minecraft.data.recipes.RecipeOutput;
+import net.minecraft.data.recipes.RecipeProvider;
+import net.minecraft.data.recipes.ShapedRecipeBuilder;
+import net.minecraft.world.item.Items;
+import net.neoforged.neoforge.common.conditions.IConditionBuilder;
+import net.wili.wilispikmins.item.ModItems;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModRecipeProvider extends RecipeProvider implements IConditionBuilder {
+    public ModRecipeProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
+        super(output, registries);
+    }
+
+    @Override
+    protected void buildRecipes(@NotNull RecipeOutput recipeOutput) {
+        ShapedRecipeBuilder.shaped(RecipeCategory.TOOLS, ModItems.WHISTLE.get())
+                .pattern(" N")
+                .pattern("I ")
+                .define('I', Items.IRON_INGOT)
+                .define('N', Items.IRON_NUGGET)
+                .unlockedBy("has_iron_ingot", has(Items.IRON_INGOT))
+                .save(recipeOutput);
+
+        ShapedRecipeBuilder.shaped(RecipeCategory.TOOLS, ModItems.CHARGING_HORN.get())
+                .pattern(" C ")
+                .pattern("CGC")
+                .define('C', Items.COPPER_INGOT)
+                .define('G', Items.GOAT_HORN)
+                .unlockedBy("has_goat_horn", has(Items.GOAT_HORN))
+                .save(recipeOutput);
+    }
 }
